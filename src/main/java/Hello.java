095d7a68ab3e6ba54c6d55e1431f0f1a632d2164
Peter Horváth <86485526+peter-horvath-sonarsource@users.noreply.github.com>
2026-06-09 15:37:29 +0200

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
   public static String GREETING2 = "Hello world!";
   public static String GREETING3 = "Hello world!";
   public static String GREETING4 = "Hello world!";
+  public static String GREETING5 = "Hello world!";
   
   public static void main(String []args) {
     

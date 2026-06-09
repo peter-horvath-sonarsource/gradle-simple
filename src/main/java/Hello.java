d05cb51b9c2c5b935fee2a9b34fe936eb2465c6a
Peter Horváth <86485526+peter-horvath-sonarsource@users.noreply.github.com>
2026-06-09 13:32:57 +0200

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
   
   public static String GREETING = "Hello world!";
+  public static String GREETING2 = "Another unuused file!";
   
   public static void main(String []args) {
     
